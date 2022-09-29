ff08fecab64aaa21c4b044cc118c5c28326ee955
tkachiko <tkachikos@gmail.com>
2022-09-29 23:36:39 +0300

    change styles in hw01 task

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -58,7 +58,7 @@ const HW1 = () => {
       <div className={s2.hwTitle}>Homework #1</div>
       <div className={s2.hw}>
         {/*проверка отображения (не менять)*/}
-        <div>
+        <div className={s2.hwMessages}>
           <Message message={message0}/>
           <FriendMessage message={friendMessage0}/>
         </div>
