b5f04ed8d5e731f951330d2de5a8f3a134a2a9cd
tkachiko <tkachikos@gmail.com>
2022-10-04 22:32:22 +0300

    hw04 task

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -1,36 +1,36 @@
-import React, {ButtonHTMLAttributes, DetailedHTMLProps} from 'react'
-import s from './SuperButton.module.css'
+import React, {ButtonHTMLAttributes, DetailedHTMLProps} from 'react';
+import s from './SuperButton.module.css';
 
 // тип пропсов обычной кнопки, children в котором храниться название кнопки там уже описан
 type DefaultButtonPropsType = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>,
-    HTMLButtonElement>
+  HTMLButtonElement>
 
 type SuperButtonPropsType = DefaultButtonPropsType & {
-    xType?: string
+  xType?: string
 }
 
 const SuperButton: React.FC<SuperButtonPropsType> = (
-    {
-        xType,
-        className,
-        disabled,
-        ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
-    }
+  {
+    xType,
+    className,
+    disabled,
+    ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
+  }
 ) => {
-    const finalClassName = s.button
-        // + (disabled
-        //         ? ...
-        //         : xType === 'red'
-        //             ? ...
-        + (className ? ' ' + className : '') // задачка на смешивание классов
+  const finalClassName = s.button
+    + (disabled
+      ? s.disabled
+      : xType === 'red'
+        ? s.secondary
+        + (className ? ' ' + className : '') : s.default);// задачка на смешивание классов
 
-    return (
-        <button
-            disabled={disabled}
-            className={finalClassName}
-            {...restProps} // отдаём кнопке остальные пропсы если они есть (children там внутри)
-        />
-    )
-}
+  return (
+    <button
+      disabled={disabled}
+      className={finalClassName}
+      {...restProps} // отдаём кнопке остальные пропсы если они есть (children там внутри)
+    />
+  );
+};
 
-export default SuperButton
+export default SuperButton;
