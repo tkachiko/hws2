8ee0906b678deb5f5e4d7c2b802bb066aaf300c0
tkachiko <tkachikos@gmail.com>
2022-11-04 19:42:24 +0300

    complete hw08 task

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,20 @@
-import {UserType} from '../HW8'
+import {UserType} from '../HW8';
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+  | { type: 'sort'; payload: 'up' | 'down' }
+  | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
-        }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
+export const homeWorkReducer = (state: UserType[], action: ActionType): any => { // need to fix any
+  switch (action.type) {
+    case 'sort': { // by name
+      const stateCopy = [...state];
+      stateCopy.sort((a, b) => action.payload === 'up' ? b.age - a.age : a.age - b.age);
+      return stateCopy; // need to fix
     }
-}
+    case 'check': {
+      return [...state.filter(u => u.age > 18)]; // need to fix
+    }
+    default:
+      return state;
+  }
+};
