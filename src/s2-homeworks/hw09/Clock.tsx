cd8ca9d337fa67091bae6363c5127c89022f7a29
tkachiko <tkachikos@gmail.com>
2022-11-12 23:54:48 +0300

    refactor hw09 task

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -13,7 +13,7 @@ function Clock() {
     // пишут студенты // запустить часы (должно отображаться реальное время, а не +1)
     // сохранить ид таймера (https://learn.javascript.ru/settimeout-setinterval#setinterval)
     stop()
-    const id: number = window.setInterval(() => setDate(new Date(restoreState('hw9-date', Date.now()))), 1000)
+    const id: number = window.setInterval(() => setDate(new Date()), 1000)
     setTimerId(id)
   };
 
@@ -32,10 +32,8 @@ function Clock() {
 
   const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-  const stringTime = ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2) + ':' + ('0' + date.getSeconds()).slice(-2) ||
-    <br/>; // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-  const stringDate = ('0' + date.getDate()).slice(-2) + '.' + ('0' + (date.getMonth() + 1)).slice(-2) + '.' +
-    date.getFullYear() || <br/>; // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+  const stringTime = date?.toLocaleTimeString() || <br/>; // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
+  const stringDate = date?.toLocaleDateString() || <br/>; // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
   // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
   const stringDay = weekday[date.getDay()] || <br/>; // пишут студенты
